f9c19715e5d91f7454d2fdad2a7c8049d3051fc1
SankrityaT <sthaku20@asu.edu>
2025-12-07 00:53:02 -0700

    Initial Commit

---
+++ SwiftMem/SwiftMemApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftMemApp.swift
+//  SwiftMem
+//
+//  Created by Sankritya Thakur on 12/7/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftMemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
