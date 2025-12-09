12521d1df5200db6d42db389df2ddd7270748328
SankrityaT <sthaku20@asu.edu>
2025-12-09 00:39:53 -0700

    Swift package: SwiftMemClient + OnDeviceCatalyst adapter

---
+++ Package.swift
@@ -0,0 +1,36 @@
+// swift-tools-version:5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+//
+// SwiftMem Swift Package manifest
+// Created by Sankritya on 12/9/25.
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftMem",
+    platforms: [
+        .iOS(.v16),
+        .macOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "SwiftMem",
+            targets: ["SwiftMem"]
+        )
+    ],
+    dependencies: [
+        // OnDeviceCatalyst is optional; SwiftMem compiles without it.
+        // If your app also depends on OnDeviceCatalyst, `OnDeviceCatalystEmbedder`
+        // will automatically be available via `#if canImport(OnDeviceCatalyst)`.
+    ],
+    targets: [
+        .target(
+            name: "SwiftMem",
+            path: "SwiftMem/SwiftMem",
+            exclude: [
+                // Exclude the app entrypoint; the package is a pure library.
+                "SwiftMemApp.swift"
+            ]
+        )
+    ]
+)
